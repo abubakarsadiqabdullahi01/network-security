c44cd59cf6f9451308016ac545e336a1510b9e2e
abubakarsadiqabdullahi001 <abdullahiabubakarsadiq3107@gmail.com>
2024-12-04 06:23:45 +0100

    fix env

--- next.config.ts
+++ next.config.ts
@@ -7,6 +7,13 @@ const nextConfig: NextConfig = {
   eslint: {
     ignoreDuringBuilds: true, // Ignores ESLint errors during build
   },
+  env: {
+    AUTH_SECRET: process.env.AUTH_SECRET,
+    DATABASE_URL: process.env.DATABASE_URL,
+    GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
+    GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
+    VIRUSTOTAL_API_KEY: process.env.VIRUSTOTAL_API_KEY,
+  },
 };
 
 export default nextConfig;
